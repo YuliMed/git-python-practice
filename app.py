6ca624f6f6dfa2c7be65b8f18528347098175cf1
YuliaDov <yuliaa@fieldintech.com>
2026-01-18 13:23:01 +0200

    Changes in feature/add-time

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 
 def greet(name):
     now = datetime.datetime.now()
-    return f"Hello, {name}! Current time is {now.time()}"
+    return f"Hello, {name}! Current time was {now.time()}"
 
 
 if __name__ == "__main__":
